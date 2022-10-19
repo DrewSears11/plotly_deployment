11377eb7fdc31acf325e6a86f56fcaf6bfc49a2c
DrewSears11 <108240844+DrewSears11@users.noreply.github.com>
2022-10-19 13:52:43 -0400

    Add files via upload

--- charts.js
+++ charts.js
@@ -122,32 +122,36 @@ function buildCharts(sample) {
   
 
     // 1. Create a variable that filters the metadata array for the object with the desired sample number.
-      var metadata = samples.filter(sampleObj => sampleObj.id == sample);
-    
-      // 2. Create a variable that holds the first sample in the metadata array.
-      var metadatasample = metadata[0];
+      var metadataArray = data.metadata.filter(sampleObj => sampleObj.id == sample);
+
+      
+    // 2. Create a variable that holds the first sample in the metadata array.
+      var metadata = metadataArray[0];
 
       // 3. Create a variable that holds the washing frequency.
-      var wfreq = parseFloat(metadatasample.wfreq)
+      var wfreq = parseFloat(metadata.wfreq)
       
       // 4. Create the trace for the gauge chart.
-      var gaugeData = [{
-      value: wfreq,
-      title: {text: "Belly Button Washing Frequency"},
-      type: "indicator",
-      mode: "gauge+number",
-      gauge: {
-        axis: {range: [null, 10]},
-        steps: [
-          { range: [0, 2], color: "red" },
-          { range: [2, 4], color: "orange" },
-          { range: [4, 6], color: "yellow" },
-          { range: [6, 8], color: "green" },
-          { range: [8, 10], color: "blue" }
-        ],
-        bar: { color: "black" },
-      },
-    }];
+      var gaugeData = [
+        {
+          domain: { x: [0, 1], y: [0, 1] },
+          value: wfreq,
+          title: { text: "<b>Belly Button Washing Frequency</b> <br> Scrubs per Week"},
+          type: "indicator",
+          mode: "gauge+number",
+          gauge: {
+            axis: { range: [null, 10] },
+            bar: { color: "black"},
+            steps: [
+              { range: [0, 2], color: "red" },
+              { range: [2, 4], color: "orange" },
+              { range: [4, 6], color: "yellow" },
+              { range: [6, 8], color: "yellowgreen" },
+              { range: [8, 10], color: "green" }
+            ],
+          }
+        }
+      ];
     
     // 5. Create the layout for the gauge chart.
     var gaugeLayout = {  width: 600, height: 450, margin: { t: 0, b: 0 }
